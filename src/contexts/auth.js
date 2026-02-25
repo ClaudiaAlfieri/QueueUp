ea0ba2e339940de8f72d61f2db2f18fbba8f918b
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-02-25 14:41:49 +0000

    Create ContextAPI

---
+++ src/contexts/auth.js
@@ -0,0 +1,20 @@
+import { useState, createContext, useEffect} from 'react'
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children}){
+    const [user, setUser] = useState(null)
+
+    return(
+        <AuthContext.Provider
+            value={{
+                signed: !!user,
+                user,
+            }}
+        >
+            { children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;

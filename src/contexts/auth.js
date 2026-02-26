0ae5f9f9b0b61ff8a4ba0c59c815a9f3f5d6b100
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-02-26 18:31:47 +0000

    Update ContextAPI

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,9 +1,15 @@
 import { useState, createContext, useEffect} from 'react'
+import { auth, db } from '../services/firebaseConnection'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, getDoc, setDoc } from 'firebase/firestore'
+
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children}){
     const [user, setUser] = useState(null)
+    const [loadingAuth, setLoadingAuth] = useState(false);
+
 
     function signIn(email, password){
         console.log(email)
@@ -11,12 +17,44 @@ function AuthProvider({ children}){
         alert("Logado com sucesso!")
     }
 
+    //Cadastrar novo user
+    async function signUp(email, password, name){
+        setLoadingAuth(true);
+
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then( async (value) => {
+            let uid = value.user.uid
+
+            await setDoc(doc(db, "users", uid), {
+                nome: name,
+                avatarUrl: null
+            })
+            .then ( () =>{
+                let data ={
+                    uid: uid,
+                    nome: name,
+                    email: value.user.email,
+                    avatarUrl: null
+                };
+                setUser(data);
+
+                setLoadingAuth(false);
+            })
+        })
+        .catch( (error) =>{
+            alert("Erro ao cadastrar usuário" + error);
+            setLoadingAuth(false);
+        })
+    }
+
     return(
         <AuthContext.Provider
             value={{
                 signed: !!user,
                 user,
-                signIn
+                signIn,
+                signUp,
+                loadingAuth
             }}
         >
             { children}

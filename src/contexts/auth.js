411024be504736974240a3405874b6e67c575345
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-02 16:15:04 +0000

    Create SignIn function

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect} from 'react'
 import { auth, db } from '../services/firebaseConnection'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 
 import { useNavigate } from 'react-router-dom'
@@ -16,10 +16,33 @@ function AuthProvider({ children}){
     const navigate = useNavigate();
 
 
-    function signIn(email, password){
-        console.log(email)
-        console.log(password);
-        alert("Logado com sucesso!")
+    async function signIn(email, password){
+        setLoadingAuth(true);
+
+         await signInWithEmailAndPassword(auth, email, password)
+        .then( async(value) =>{
+            let uid = value.user.uid;
+
+            const docRef = doc(db, "users", uid);
+            const docSnap =  await getDoc(docRef)
+
+            let data= {
+                uid: uid,
+                nome: docSnap.data().nome,
+                email: value.user.email,
+                avatarUrl: docSnap.data().avatarUrl
+            }
+
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+            toast.success("Bem-vindo(a) de volta!")
+            navigate("/dashboard")
+        })
+         .catch( (error) =>{       
+            setLoadingAuth(false);
+            toast.error("Ops algo deu errado!")
+        })
     }
 
     //Cadastrar novo user
@@ -48,9 +71,9 @@ function AuthProvider({ children}){
                 navigate("/dashboard")
             })
         })
-        .catch( (error) =>{
-            alert("Erro ao cadastrar usuário" + error);
+        .catch( (error) =>{        
             setLoadingAuth(false);
+            toast.error("Ops algo deu errado!")
         })
     }
 

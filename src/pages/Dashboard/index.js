be24514d9c6cf8e31ecaf7b0a18cb4b7fa4841be
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-10 15:58:57 +0000

    Update Dashboard.js

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -43,9 +43,31 @@ export default function Dashboard(){
             </thead>
             <tbody>
               <tr>
-                <td data-label="Cliente"> Mercado Esquina</td>
+                <td data-label="Cliente">Mercado Esquina</td>
                 <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">Em aberto</td>
+                <td data-label="Status">
+                  <span className="badge" style={{backgroundColor:'#999'}}>
+                    Em aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado em">12/05/2022</td>
+                <td data-label="#"></td>
+                  <button className="action" style={{backgroundColor:'#3583f6'}}>
+                    <FiSearch color="#fff" size={17}/>
+                  </button>
+                  <button className="action" style={{backgroundColor:'#f6a935'}}>
+                    <FiEdit2 color="#fff" size={17}/>
+                  </button>
+              </tr>
+
+               <tr>
+                <td data-label="Cliente">Informática TEC</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{backgroundColor:'#999'}}>
+                    Em aberto
+                  </span>
+                </td>
                 <td data-label="Cadastrado em">12/05/2022</td>
                 <td data-label="#"></td>
                   <button className="action" style={{backgroundColor:'#3583f6'}}>

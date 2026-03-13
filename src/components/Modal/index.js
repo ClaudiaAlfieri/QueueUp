8fe01c5ee8027f11df98510634695872cfa36b6f
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-13 12:09:08 +0000

    Update Modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,48 +1,54 @@
 
 import { FiX } from 'react-icons/fi'
-import './modal.css'
+import './modal.css';
 
-export default function Modal(){
-    return(
-        <div className="modal">
-            <div className="container">
-                <button className="close">
-                    <FiX size={25} color="#FFF" />
-                    Voltar
-                </button>
-                <main>
-                    <h2>Detalhes do chamado</h2>
+export default function Modal({ conteudo, close }){
+  return(
+    <div className="modal">
+      <div className="container">
+        <button className="close" onClick={ close }>
+          <FiX size={25} color="#FFF" />
+          Voltar
+        </button>
 
-                    <div className="row">
-                        <span>
-                            Cliente: <i>Mercado</i>
-                        </span>
-                    </div>
-                    
-                    <div className="row">
-                        <span>
-                            Assunto: <i>Suporte</i>
-                        </span>
-                        <span>
-                            Cadastrado em: <i>22/08/2022</i>
-                        </span>
-                    </div>
+        <main>
+          <h2>Detalhes do chamado</h2>
 
-                    <div className="row">
-                        <span>
-                            Status: <i>Aberto</i>
-                        </span>
-                    </div>
+          <div className="row">
+            <span>
+              Cliente: <i>{conteudo.cliente}</i>
+            </span>
+          </div>
 
-                    <>
-                        <h3>Complemento</h3>
-                        <p>
-                            Aqui vai todo o complemento do chamado
-                        </p>
-                    </>
+          <div className="row">
+            <span>
+              Assunto: <i>{conteudo.assunto}</i>
+            </span>
+            <span>
+              Cadastrado em: <i>{conteudo.createdFormat}</i>
+            </span>
+          </div>
 
-                </main>
-            </div>            
-        </div>
-    )
+          <div className="row">
+            <span>
+              Status: 
+              <i className="status-badge" style={{ color: "#FFF", backgroundColor: conteudo.status === 'Aberto' ? '#5cb85c' : '#999' }}>
+                {conteudo.status}
+              </i>
+            </span>
+          </div>
+
+          {conteudo.complemento !== '' && (
+          <>
+            <h3>Complemento</h3>
+            <p>
+              {conteudo.complemento}
+            </p>
+          </>
+          )}
+
+        </main>
+      </div>
+    </div>
+  )
 }
